9b10ab0ddcd9b32b94f24245e999c22b21e9cf1d
k-bar-r <akbar@stmik-wp.ac.id>
2025-03-16 14:27:54 +0700

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Livewire Example</title>
+
+    @livewireStyles
+</head>
+<body>
+    <h1>Halo, Livewire!</h1>
+
+    {{-- Tambahkan komponen Livewire di dalam body --}}
+    <livewire:live-component />
+
+    @livewireScripts
+</body>
+</html>
